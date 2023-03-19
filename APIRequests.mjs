eb9ab1a87bdbccb1aed9c3364fe48d2dd07c3c38
isan-san <isan.9.9f@gmail.com>
2023-03-19 16:21:39 -0500

    Add calculator type to fetch request

--- APIRequests.mjs
+++ APIRequests.mjs
@@ -1,4 +1,5 @@
 export const CALCULATE_SLAB = (data) => {
+  data.calculator = "slab-our";
   return new Promise((resolve, reject) => {
     fetch("https://7wuv3g8kx5.execute-api.sa-east-1.amazonaws.com/Test/", {
       method: "POST",
